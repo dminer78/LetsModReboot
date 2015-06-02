6b213c1f483a901baa9e3b3c9c6547eeb5afe932
unknown <dminer78@users.noreply.github.com>
2015-06-01 18:04:09 -0700

    Up to episode 7.5 I HAS CONFIG GOOEY!! \o/

--- src/main/java/com/dminer78/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/dminer78/letsmodreboot/LetsModReboot.java
@@ -1,15 +1,17 @@
 package com.dminer78.letsmodreboot;
 
-import com.dminer78.letsmodreboot.configuration.ConfigurationHandler;
+import com.dminer78.letsmodreboot.handler.ConfigurationHandler;
 import com.dminer78.letsmodreboot.proxy.IProxy;
 import com.dminer78.letsmodreboot.reference.Reference;
+import com.dminer78.letsmodreboot.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot {
 
     @Mod.Instance(Reference.MOD_ID) //Arg is modID
@@ -25,15 +27,23 @@ public class LetsModReboot {
         //Initialize Items, Blocks, and Config
 
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+
+        LogHelper.info("Pre Initialization Complete!");
+
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         //Register GUI, Crafting Recipes, and Tile Entities
+
+        LogHelper.info("Initialization Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         //Anything you want to run after other mods have loaded
+
+        LogHelper.info("Post Initialization Complete!");
     }
 }
